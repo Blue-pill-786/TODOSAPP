3703f45793e226af39d1596681db6ed65a87ef2f
Blue-pill-786 <113605539+Blue-pill-786@users.noreply.github.com>
2024-05-25 22:44:35 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,18 @@
 
+import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
 
 function Home(){
 
     return(
         <div className={styles.container}>
-            <a href="/todo" className={"btn btn-warning "+styles.link}>
+            {/* <a href="/todo" className={"btn btn-warning "+styles.link}>
                 To Do App
-            </a>
+            </a> */}
+            <div className={"btn btn-warning "+styles.link} >
+
+            <Link to='/todo'>To do App</Link>
+            </div>
             <a href="notes" className={"btn btn-warning "+styles.link}>
                 Note Keeper
             </a>
